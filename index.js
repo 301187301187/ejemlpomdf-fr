b750597b190b405d8fc9a72b9afdb519bf1b7952
301187301187 <151100839+301187301187@users.noreply.github.com>
2023-11-24 00:55:04 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var bandera =false;
+
+function validar(){
+    if (bandera){
+        console.log("Validacion completa")
+        //redireccion ala pagina correspondiente
+    }
+
+}
+function correctCaptcha(){
+    console.log("Captcha resuelte correctamente")
+    bandera = true;
+}
